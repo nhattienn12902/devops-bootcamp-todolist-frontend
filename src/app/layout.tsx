a348561f8afd0d1caa30961489370dfc32ca0b2a
nhattienn12902 <nhattienn12902@gmail.com>
2026-05-08 23:05:49 +0700

    test deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
-// commit test
+// commit test 123
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
